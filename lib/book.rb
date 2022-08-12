0c40b8c7b75ac1aa3d463c46a1fa9dcb22c7ffed
Laura Gonzalez <lg458@cornell.edu>
2022-08-12 14:48:35 -0700

    commit tree c68ec826347172c890a7b6105e7bcdc6c328ebc3

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
+class Book
+  attr_reader :title 
+  attr_accessor :author
+  attr_accessor :page_count 
+  attr_accessor :genre
 
+  def initialize(title) 
+    @title = title
+    @author = author
+    @page_count = page_count
+    @genre = genre
+  end 
 
+  def turn_page 
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
